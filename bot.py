90dfea1c64c8f420e3fbd9d6080ff860f8429645
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-08-08 23:54:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.types import ForceReply
-import subprocess
-from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
+from os import system as cmd
+import shutil
 
 bot = Client(
     "voluemincreaser",
@@ -11,19 +10,10 @@ bot = Client(
     bot_token="6280972722:AAG3GrropPJhZvfjljtgppKeeXpfpBVZG4Y"
 )
 
-CHOOSE_UR_LANG = " اختر النمط الخاص بإزالة الضوضاء "
-CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("mod1",callback_data="mod1")],
-     [InlineKeyboardButton("mod2",callback_data="mod2")],
-     [InlineKeyboardButton("mod3",callback_data="mod3")],
-     [InlineKeyboardButton("mod4",callback_data="mod4")],
-     [InlineKeyboardButton("mod5",callback_data="mod5")]
-]
-
 
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا إزالة الضوضاء ورفع مستوى الصوت , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " السلام عليكم أنا إزالة الضوضاء ورفع مستوى الصوت , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/sunnay6626/2 ",disable_web_page_preview=True)
 
 @bot.on_message(filters.private & filters.incoming &  filters.audio | filters.voice  )
 def _telegram_file(client, message):
@@ -31,44 +21,19 @@ def _telegram_file(client, message):
   global user_id
   user_id = message.from_user.id 
   file = message
-  global file_path
   file_path = message.download(file_name="./downloads/")
   filename = os.path.basename(file_path)
   realname, ext = os.path.splitext(filename)
-  global mp3file
-  mp3file = realname+".mp3"
-  message.reply(
-             text = CHOOSE_UR_LANG,
-             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_LANG_BUTTONS)
-
-        )
-@bot.on_callback_query()
-def callback_query(CLIENT,CallbackQuery):
-  global langtoken
-  if CallbackQuery.data == "mod1":
-      langtoken = "./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn"
-  elif CallbackQuery.data == "mod2":
-      langtoken = "./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn"
-  elif CallbackQuery.data == "mod3":
-      langtoken = "./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn"
-  elif CallbackQuery.data == "mod4" :
-      langtoken = './rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn'
-  elif CallbackQuery.data == "mod5":
-      langtoken = "./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn"
-  
-  CallbackQuery.edit_message_text(
-      
-      "جار إزالة الضوضاء"
-  )   
-  subprocess.call(['ffmpeg','-i',file_path,'-af','arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn',"mod"+mp3file,'-y']) 
-  subprocess.call(['ffmpeg','-i',"mod"+mp3file,'-af', "volume=2",mp3file,'-y']) 
+  mp3file = f'''{realname}.mp3'''
+  tempmp3 = f'''mod{mp3file}'''
+  cmd(f'ffmpeg -i {file_path} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y')
+  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
+  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn ./downloads/{tempmp3} -y ')
+  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn ./downloads/{mp3file} -y ')
+  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn ./downloads/{tempmp3} -y ')
+  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn ./downloads/{mp3file} -y ')
   with open(mp3file, 'rb') as f:
          bot.send_audio(user_id, f)
-  subprocess.call(['unlink',mp3file]) 
-  subprocess.call(['unlink',"mod"+mp3file])
-  subprocess.call(['unlink',file_path]) 
-
-
-       
-
+  shutil.rmtree('./downloads/')
+  
 bot.run()
